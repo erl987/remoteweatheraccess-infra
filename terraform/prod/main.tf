cf882007e45a3b72ba40c83b03f435d353553a79
Ralf Rettig <meteoradix@gmx.net>
2021-05-09 15:07:07 +0000

    Resolve "Remove all hard-coded values from Terraform configuration"

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -15,37 +15,38 @@ terraform {
   }
 }
 
-variable "project_id" {}
-variable "region" {}
-variable "database_version" {}
-variable "sql_backup_region" {}
-variable "database_tier" {}
+# provided as environment variables `TF_VAR_gcp_project_id`, etc. in Gitlab
+variable "gcp_project_id" {}
+variable "gcp_region" {}
+variable "gcp_database_version" {}
+variable "gcp_sql_backup_region" {}
+variable "gcp_database_tier" {}
 
 provider "random" {}
 
 # the credentials have to be provided in the environment variable `GOOGLE_APPLICATION_CREDENTIALS`
 provider "google" {
-  project = var.project_id
-  region = var.region
+  project = var.gcp_project_id
+  region = var.gcp_region
 }
 
 module "gcp_api_activation" {
   source = "../modules/gcp-api-activation"
-  project_id = var.project_id
+  project_id = var.gcp_project_id
 }
 
 module "gcp_cloud_sql" {
   source = "../modules/gcp-cloud-sql"
-  region = var.region
-  database_version = var.database_version
-  sql_backup_region = var.sql_backup_region
-  database_tier = var.database_tier
+  region = var.gcp_region
+  database_version = var.gcp_database_version
+  sql_backup_region = var.gcp_sql_backup_region
+  database_tier = var.gcp_database_tier
   depends_on = [module.gcp_api_activation]
 }
 
 module "gcp_secret_manager" {
   source = "../modules/gcp-secret-manager"
-  region = var.region
+  region = var.gcp_region
   user-db-password = module.gcp_cloud_sql.user-db-password
   weatherdata-db-password = module.gcp_cloud_sql.weatherdata-db-password
   depends_on = [module.gcp_api_activation]
@@ -53,5 +54,5 @@ module "gcp_secret_manager" {
 
 module "gcp_service_accounts" {
   source = "../modules/gcp-service-accounts"
-  project_id = var.project_id
+  project_id = var.gcp_project_id
 }
