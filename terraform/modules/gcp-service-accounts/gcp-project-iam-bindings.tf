c7271586e001bedfd750fdf4bbb35872c25b31f5
Ralf Rettig <meteoradix@gmx.net>
2024-02-06 19:10:44 +0100

    Make exporter also runnable locally

--- terraform/modules/gcp-service-accounts/gcp-project-iam-bindings.tf
+++ terraform/modules/gcp-service-accounts/gcp-project-iam-bindings.tf
@@ -110,5 +110,6 @@ resource "google_project_iam_binding" "frontend-log-writer-role" {
 
   members = [
     "serviceAccount:${google_service_account.frontend-service-account.email}",
+    "serviceAccount:${google_service_account.exporter-service-account.email}",
   ]
 }
